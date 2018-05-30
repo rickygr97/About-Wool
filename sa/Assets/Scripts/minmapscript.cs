c7c33e577298bbd23d0d3a367afdbb43e8554652
rickygr97 <rick.de.graaf@live.nl>
2018-05-30 09:58:17 +0100

    change

---
+++ sa/Assets/Scripts/minmapscript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class minmapscript : MonoBehaviour {
+
+    public Transform pl;
+
+
+    private void LateUpdate()
+    {
+        Vector3 mewposition = pl.position;
+        mewposition.y = transform.position.y;
+        transform.position = mewposition;
+    }
+}
